e6aa078cfbeb0c56804191c442091cceb4b3af5a
an156657 <156647069+an156657@users.noreply.github.com>
2024-09-16 07:07:58 +0000

    boolean.js Aufgaben erstellt, objekte.js angelegt

--- uebungen/02-boolean.js
+++ uebungen/02-boolean.js
@@ -12,11 +12,26 @@ if(true){
 }else{
     console.log("Der Ausdruck ist false.")}
 
+    // Weil in dieser Aufgabe die Prüfung immer wahr ist if(true), wird immer die Anweisung hinter if ausgeführt. 
+    // Die Anweisung hinter else kommt niemals zur Ausführung.
+
 console.log("Aufgabe 2");
 
 // In Erweiterung von Aufgabe 1 soll der Ausdruck dynamisch entweder true oder false sein.
 
 let a = 7
+
+// Hier wird mit einem Prüfoperator auf wahr oder falsch geprüft.
+
+// Prüfoperatoren: 
+// ==   : Prüfung auf Gleichheit im Wert Bsp.: if (1 == '1') true
+// ===  : Prüfung auf Gleichheit im Wert und im Typ Bsp.: if (1 === '1') false
+// >    : größer als
+// <    : kleiner als
+// >=   : größer oder gleich
+// <=   : kleiner oder gleich
+
+
 if(a>6){console.log("Das ist wahr")}
 else{console.log("Das ist falsch")}
 
@@ -46,3 +61,54 @@ if(fuehrerschein){ // Führerschein ist bereits true oder false. Es bedarf keine
 
 }else{console.log("Fahrrad fahren")
 }
+
+console.log('Aufgabe 4')
+// Zahlen können in Javascript true oder false sein.
+
+let zahl = 0;
+
+if (zahl) { console.log('Die Zahl ' +zahl+ ' nimmt den boolschen Wert true an.')
+} else {console.log('Die Zahl ' +zahl+ ' nimmt den boolschen Wert false an.')}
+
+// Positive Zahlen sind true.
+// Die Ziffer Null ist false.
+// Negative Zahlen sind true.
+
+console.log('Aufgabe 5')
+
+// Auf einem Sparkonto soll bei einem positiven Kontostand auf der Konsole stehen: "Zinsen berechenen"
+// Wenn kein Guthaben existiert: "keine Zinsen"
+
+let kontostand = 10;
+
+if(kontostand){ // Auf den Prüfoperator kann verzichtet werden, da Zahlen bereits true oder false sind.
+    console.log('Zinsen berechnen für Kontostand ' +kontostand)}
+else{console.log('keine Zinsen, da Kontostand 0')}
+
+console.log('Aufgabe 6')
+// Wenn die Temperatur positiv ist, soll auf der Konsole stehen: "nach draußen gehen"
+// Wenn die Temperatur 0°C beträgt: "drinnen bleiben"
+
+let temperatur = 10;
+
+if(temperatur){
+    console.log('Bei ' +temperatur+ '°C nach draußen gehen')}
+else{console.log('Bei ' +temperatur+ '°C drinnen bleiben.')}
+
+console.log('Aufgabe6.1')
+// In der Bundesliga gibt es Punkte für Spiele.
+// Sieg:            3 Punkte
+// Unentschieden:   1 Punkt
+// Niederlage:      0 Punkte
+
+let spielausgang = "niederlage";
+let mannschaft = "Deutschland";
+
+if(spielausgang == 'sieg'){
+    console.log('Es gibt 3 Punkte.')}
+
+if(spielausgang == 'unentschieden'){
+    console.log('Es gibt 1 Punkt.')}
+
+if(spielausgang == 'niederlage'){
+    console.log('Es gibt 0 Punkte.')}
